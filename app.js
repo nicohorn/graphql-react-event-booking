d0ae2d16650787732d5c569c5df55f2bf73ce7ce
Nicolás Horn <nicoskate000@gmail.com>
2021-10-18 17:29:05 -0300

    finished 5th video from the series (YouTube channel: Academind)

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
 
 const app = express();
-const Event = require('./models/event')
+const Event = require('./models/event');
+const { events } = require('./models/event');
 
 app.use(bodyParser.json());
 app.use('/graphql',
@@ -40,7 +41,17 @@ app.use('/graphql',
         rootValue: {
             events: () => {
                 //this find method is provided by mongoose. if we execute the method with no filters, it'll retrieve all the Event objects it finds.
-                Event.find()
+                return Event.find()
+                    .then(events => {
+                        //this mapping is done to leave out all the metadata that mongoose generates. this is done using the _doc property (also provided by mongoose)
+                        return events.map(event => {
+                            //this _id part is to overwrite the original _id retrieved in _doc, becasue GraphQL doesn't "understand" the special datatype that _id is originally.
+                            return {...event._doc, _id: event._doc._id.toString() };
+                        });
+                    }).catch(err => {
+                        console.log(err);
+                        throw err;
+                    })
             },
             createEvent: (args) => {
 
@@ -54,7 +65,7 @@ app.use('/graphql',
                 //due to the GraphQL configuration, this function/method must return an object
                 return event.save().then(result => {
                     console.log(result)
-                    return {...result._doc }
+                    return {...result._doc, _id: event._doc._id.toString() }
                     //_doc is a property provided by mongoose which give all the core properties that make the object and leave out all the metadata
                 }).catch(err => {
                     console.log(err)
